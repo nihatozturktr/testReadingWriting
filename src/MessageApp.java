c7c569100ecdde812b31d7f86054216c82c62243
nihatozturktr <nihatozturktr@gmail.com>
2022-04-29 16:09:55 +0200

    Last Commit

--- src/MessageApp.java
+++ src/MessageApp.java
@@ -1,2 +1,94 @@
-package PACKAGE_NAME;public class MessageApp {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class MessageApp {
+    public static void main(String[] args) {
+
+
+
+        Path messagePath = Path.of("/Users/nihat/FromNihatOzturkToPearl/Message.txt");
+        Path animalPath = Path.of("/Users/nihat/FromNihatOzturkToPearl/Animal.txt");
+        ///Users
+
+        try {
+            if (Files.notExists(messagePath.getParent())) {
+                Files.createDirectory(messagePath.getParent());
+                Files.createDirectory(animalPath.getParent());
+
+            }
+
+            if(Files.notExists(messagePath)){
+                Files.createFile(messagePath);
+                Files.createFile(animalPath);
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter(messagePath.toString())
+        )){
+
+            bufferedWriter.write("Welk kledingstuk denkt alleen maar aan zichzelf?\n" +
+                    "Antwoord: Asosjaal.”");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            FileWriter fileWriter = new FileWriter(String.valueOf(messagePath));
+            fileWriter.write("Live as if you will die tomorrow, learn as if you will live forever.");
+            fileWriter.write("Mahatma Gandhi");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(messagePath)))){
+            String line=null;
+            while ((line = reader.readLine())!=null){
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("----------ANIMAL PATH-------------");
+
+
+        Pokemon pokemon = new Pokemon("Pidgeot",false);
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter(animalPath.toString())
+        )){
+
+            bufferedWriter.write(pokemon.getName()+" "+pokemon.getEdible());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (FileReader fileReader = new FileReader(animalPath.toString());
+             BufferedReader bufferedReader = new BufferedReader(fileReader)){
+
+            String line;
+            while ((line= bufferedReader.readLine())!=null){
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+    }
+
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Pokemon {
+public class Pokemon {
+    private String name;
+    private boolean edible;
+
+
+
+    public Pokemon(String name, boolean edible) {
+        this.name = name;
+        this.edible = edible;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getEdible() {
+        return edible;
+    }
+
+    public void setEdible(boolean edible) {
+        this.edible = edible;
+    }
 }
